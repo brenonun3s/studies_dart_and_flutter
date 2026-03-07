1f3a514d99f8178c174d4933db9fc9ccbd2b6816
Breno Nunes <morais.brenonunes@hotmail.com>
2026-03-07 20:41:20 -0300

    feat: first commit, initialized studies in dart. Exists some errors in the code pendind of fix

---
+++ cli/bin/cli.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:command_runner/command_runner.dart';
+
+const version = '0.1.9';
+
+void main(List<String> arguments) {
+  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+  commandRunner.run(arguments);
+}
+
+void printUsage() {
+  print(
+    "The following commands are available: 'help, 'verson', 'search <ARTICLE-TITLE>'",
+  );
+}
+
+void searchWikipedia(List<String>? arguments) async {
+  final String articleTitle;
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    final inputFromStdin = stdin.readLineSync();
+    if (inputFromStdin == null || inputFromStdin.isEmpty) {
+      print('No article title provided. Exiting.');
+      return;
+    }
+    articleTitle = inputFromStdin;
+  } else {
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+
+  var articleContent = await getWikipediaArticle(articleTitle);
+  print(articleContent);
+  Future<String> getWikipediaArticle(String articleTitle) async {
+    final url = Uri.https(
+      'en.wikipedia.org',
+      '/api/rest_v1/page/summary/$articleTitle',
+    );
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      return response.body;
+    }
+
+    return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
+  }
+}
